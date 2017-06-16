916fdbea8d7edc1a7c2dd383a050d0e100346574
kasradzenika <kasradzenika@yahoo.com>
2017-06-16 11:49:40 +0400

    initial commit: creating cubes on target

---
+++ Assets/Scripts/TouchExpolsionController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class TouchExpolsionController : MonoBehaviour
+{
+    public GameObject explosionObject;
+
+	void Update ()
+    {
+        ClickChecker();
+    }
+
+    void ClickChecker()
+    {
+        //on click/touch create an explosion effect on a Collider
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray charles = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInformation;
+            bool hit = Physics.Raycast(charles, out hitInformation);
+
+            if (hit)
+            {
+                GameObject explosionCloneObjectThatICreatedOnLine21 = Instantiate(explosionObject);
+                explosionCloneObjectThatICreatedOnLine21.transform.position = hitInformation.point;
+                explosionCloneObjectThatICreatedOnLine21.transform.up = hitInformation.normal;
+
+                Destroy(explosionCloneObjectThatICreatedOnLine21, 5f);
+            }
+        }
+    }
+}
